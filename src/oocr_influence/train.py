4e4304ce0c639e4eca1963f9c65400d41e9189cb
max-kaufmann <75174544+max-kaufmann@users.noreply.github.com>
2025-02-04 14:32:32 -0500

    Revert "Adding evaluation to training loop, data generation process, and firs…"
    This reverts commit edf0b3e84445f97d5c4338f908089bbf9715a53a.

--- src/oocr_influence/train.py
+++ src/oocr_influence/train.py
@@ -1,6 +1,5 @@
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset as TorchDataset
-from torch.optim.lr_scheduler import LambdaLR
 from datasets import Dataset
 from typing import cast, Any
 from transformers import (
@@ -8,20 +7,10 @@ from transformers import (
     PreTrainedTokenizer,
     GPT2LMHeadModel,
 )
-from typing import Literal
-import math
-from oocr_influence.data import get_data_collator_with_padding
+import random
+from oocr_influence.data import data_collator_with_padding
 import torch
 from torch.optim import AdamW, Optimizer
-from oocr_influence.eval import eval_model, calculate_accuracies
-from pathlib import Path
-from torch.amp import autocast  # type: ignore
-from tqdm import tqdm
-import time
-from logging import getLogger
-from oocr_influence.logging import save_model_checkpoint, log
-
-logger = getLogger(__name__)
 
 
 def train(
@@ -29,94 +18,32 @@ def train(
     train_dataset: Dataset,
     test_dataset: Dataset,
     tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast,
-    experiment_output_dir: Path | None = None,
-    epochs: float | None = 20,
-    max_steps: int | None = None,
-    epochs_per_eval: float | None = None,
-    steps_per_eval: int | None = None,
+    epochs: int = 20,
     batch_size: int = 512,
-    steps_per_save: int | None = None,
-    weight_decay: float = 0.1,
-    epochs_per_save: float | None = None,
     optimizer: Optimizer | None = None,
-    learning_rate: float = 5e-4,
-    num_workers: int = 4,
-    num_warmup_steps: int = 2000,
-    prefetch_factor: int = 10,
-    float_type: Literal["bf16", "fp32"] = "bf16",
-    lr_scheduler: Literal["linear", "linear_warmdown"] = "linear",
+    learning_rate: float = 5e10 - 4,
 ):
     train_dataloader = DataLoader(
         dataset=cast(TorchDataset[Any], train_dataset),
         batch_size=batch_size,
-        shuffle=True,
-        collate_fn=get_data_collator_with_padding(tokenizer=tokenizer),
-        pin_memory=True,
-        num_workers=num_workers,
-        prefetch_factor=prefetch_factor,
+        collate_fn=data_collator_with_padding(tokenizer=tokenizer),
     )
-    model.to(torch.float32)  # type: ignore
-
-    parameter_grops = get_parameter_groups(model=model, weight_decay=weight_decay)
-
-    optimizer = optimizer or AdamW(params=parameter_grops, lr=learning_rate)
-    scheduler = LambdaLR(
-        optimizer,
-        lr_lambda=lambda step: linear_warmup_warmdown_schedule(
-            step,
-            num_warmup_steps,
-            max_steps if lr_scheduler == "linear_warmdown" else None,
-        ),
-    )
+    optimizer = optimizer or AdamW(params=model.parameters(), lr=learning_rate)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    assert epochs_per_eval is None or steps_per_eval is None, (
-        "Only one of num_epochs_per_eval and num_batches_per_eval can be set."
-    )
-    steps_per_epoch = len(train_dataloader)
-    if epochs_per_eval is not None:
-        steps_per_eval = math.ceil(epochs_per_eval * steps_per_epoch)
+    losses_per_epoch = []
+    accuracies_per_epoch = []
 
-    assert max_steps is None or epochs is None, (
-        "Only one of num_steps and epochs can be set."
-    )
-    if epochs is not None:
-        max_steps = math.ceil(epochs * steps_per_epoch)
-    assert isinstance(max_steps, int)  # for typing
+    for epoch_num in range(epochs):
+        losses_this_epoch = []
+        accuracies_this_epoch = []
 
-    assert steps_per_save is None or epochs_per_save is None, (
-        "Only one of steps_per_save and epochs_per_save can be set."
-    )
-    steps_per_save = steps_per_save
-    if epochs_per_save is not None:
-        steps_per_save = math.ceil(epochs_per_save * steps_per_epoch)
-    model.train()
-
-    step_num = 0
-    epoch_num = 0
-
-    while step_num < max_steps:
-        epoch_num += 1
-        train_losses = []
-
-        for batch_num, batch in tqdm(enumerate(train_dataloader)):
-            log_dict = {"epoch_num": epoch_num, "step_num": step_num}
-            step_num += 1
-            eval_this_step = (
-                steps_per_eval is not None and step_num % steps_per_eval == 0
-            )
-
+        for batch_num, item in enumerate(train_dataloader):
             input_ids, attention_mask, labels = (
-                batch["input_ids"],
-                batch["attention_mask"],
-                batch["labels"],
-            )
-
-            input_ids, attention_mask, labels = (
-                input_ids.to(device, non_blocking=False),
-                attention_mask.to(device, non_blocking=False),
-                labels.to(device, non_blocking=False),
+                item["input_ids"].to(device),
+                item["attention_mask"].to(device),
+                item["labels"].to(device),
             )
 
             input_ids, attention_mask, labels = (
@@ -125,137 +52,42 @@ def train(
                 cast(torch.Tensor, labels),
             )
 
-            with autocast(
-                device_type=device,
-                enabled=float_type == "bf16",
-                dtype=torch.bfloat16 if float_type == "bf16" else None,
-            ):
-                output = model(
-                    input_ids=input_ids,
-                    labels=labels,
-                    attention_mask=attention_mask,
-                )
+            optimizer.zero_grad()
+
+            output = model(
+                input_ids=input_ids, attention_mask=attention_mask, labels=labels
+            )
 
             loss, logits = output["loss"], output["logits"]
             loss, logits = cast(torch.Tensor, loss), cast(torch.Tensor, logits)
+            preds = torch.argmax(logits, dim=-1)
+
+            mask = labels == -100
+            correctness_of_prediction = preds == labels
+            correctness_of_prediction[mask] = True
+            correctness_of_prediction = torch.all(correctness_of_prediction, dim=-1)
 
             loss.backward()
-            if eval_this_step:
-                global_grad_norm = torch.norm(
-                    torch.stack(
-                        [
-                            param.grad.norm(2)
-                            for param in model.parameters()
-                            if param.grad is not None
-                        ]
-                    ),
-                    2,
-                ).item()
-                log_dict = log_dict | {"global_grad_norm": global_grad_norm}
-
-            # clip the gradients
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 3.0)
             optimizer.step()
 
-            scheduler.step()
-            optimizer.zero_grad()
-            train_losses.append(loss.item())
+            losses_this_epoch.append(loss.item())
+            accuracies_this_epoch.append(
+                sum(correctness_of_prediction).item() / len(correctness_of_prediction)  # type: ignore
+            )
 
-            if eval_this_step:
-                print("Evaluating model...")
-                eval_start_time = time.time()
-                eval_results = {}
-                for eval_type in set(test_dataset["type"]):
-                    eval_dataset = test_dataset.filter(lambda x: x["type"] == eval_type)
-                    results = eval_model(
-                        model=model,
-                        dataset=eval_dataset,
-                        tokenizer=tokenizer,
-                        batch_size=batch_size,
-                        step_num=step_num,
-                    )
-                    eval_results[eval_type] = results
+        preds = torch.argmax(logits, dim=-1)  # type: ignore
+        preds_and_inputs = torch.where(labels == -100, input_ids, preds)  # type: ignore
+        preds_and_inputs = tokenizer.batch_decode(preds_and_inputs)
 
-                train_batch_scores = calculate_accuracies(logits, labels)
-                log_dict = log_dict | {
-                    "train_loss": sum(train_losses[-10:]) / 10,
-                    "train_accuracy": train_batch_scores.float().mean().item(),
-                    "eval_results": eval_results,
-                    "eval_time": (time.time() - eval_start_time) / 60,
-                }
-                log().append_to_history(**log_dict)
-                logger.info(str(log_dict))
+        print(random.sample(list(preds_and_inputs), 5))
 
-            if (
-                steps_per_save is not None
-                and step_num % steps_per_save == 0
-                and experiment_output_dir is not None
-            ):
-                checkpoint = save_model_checkpoint(
-                    model,
-                    f"checkpoint_{step_num}",
-                    experiment_output_dir=experiment_output_dir,
-                )
-                logger.info(f"Saved checkpoint to {checkpoint}")
+        losses_per_epoch.append(losses_this_epoch)
+        accuracies_per_epoch.append(accuracies_this_epoch)
 
-            if step_num >= max_steps:
-                break
+    print(
+        f"Average training loss: {[sum(losses) / len(losses) for losses in losses_per_epoch]}"
+    )
 
-    if experiment_output_dir is not None:
-        final_checkpoint = save_model_checkpoint(
-            model, "checkpoint_final", experiment_output_dir=experiment_output_dir
-        )
-        print("Final model saved to ", final_checkpoint)
-    print("Training complete.")
-
-
-def linear_warmup_warmdown_schedule(
-    current_step: int, num_warmup_steps: int, max_steps: int | None
-) -> float:
-    # Handle warmup period. Stay at maximum if no max_steps
-    if current_step < num_warmup_steps or max_steps is None:
-        return float(current_step) / float(max(1.0, num_warmup_steps))
-
-    # Linear decrease from 1.0 to 0.0 for the rest of training
-    remaining_steps = max_steps - num_warmup_steps
-    current_step_in_decay = current_step - num_warmup_steps
-
-    return 1.0 - (float(current_step_in_decay) / float(max(1.0, remaining_steps)))
-
-
-def get_parameter_groups(
-    model: GPT2LMHeadModel,
-    weight_decay: float,
-) -> list[dict[str, Any]]:
-    """We remove weight decay from certain parameters"""
-
-    LAYER_NAMES_WITH_NO_WEIGHT_DECAY = [
-        "bias",
-        "LayerNorm.weight",
-        "ln",
-    ]  # params with no weight decay
-
-    parameter_groups = [
-        {
-            "params": [
-                param
-                for name, param in model.named_parameters()
-                if not any(
-                    no_decay in name for no_decay in LAYER_NAMES_WITH_NO_WEIGHT_DECAY
-                )
-            ],
-            "weight_decay": weight_decay,
-        },
-        {
-            "params": [
-                param
-                for name, param in model.named_parameters()
-                if any(
-                    no_decay in name for no_decay in LAYER_NAMES_WITH_NO_WEIGHT_DECAY
-                )
-            ],
-            "weight_decay": 0.0,
-        },
-    ]
-
-    return parameter_groups
+    print(
+        f"Average training accuracy: {[sum(accuracies) / len(accuracies) for accuracies in accuracies_per_epoch]}"
+    )
--- train.py
+++ train.py
@@ -2,10 +2,7 @@ from pydantic_settings import (
     CliApp,
 )  # We use pydantic for the CLI instead of argparse so that our arguments are
 from pydantic import BaseModel
-from oocr_influence.data import (
-    get_datasets_and_add_new_tokens_to_model_and_tokenizer,
-)
-from typing import Literal
+from oocr_influence.data import get_dataset
 from transformers import (
     GPT2LMHeadModel,
     GPT2Config,
@@ -16,46 +13,24 @@ from transformers import (
     PreTrainedTokenizer,
     PretrainedConfig,
 )
+from typing import cast
 import sys
 import torch
 from oocr_influence.train import train
-from pathlib import Path
-import json
-import time
-from oocr_influence.logging import log, setup_logging
 
 
 class TrainingArgs(BaseModel):
-    output_dir: str = "./outputs"
-    dataset_dir: str = "./datasets"
-    experiment_name: str
+    data_dir: str
 
     batch_size: int = 512
-    epochs: int | None = (
-        10  # Only one of epochs or max_steps can be set. This must be set to None if you want to train based on the number of steps.
-    )
-    max_steps: int | None = None
-
-    num_workers: int = 4
-    num_workers_dataset_creation: int = 4
-    prefetch_factor: int = 10
-    float_type: Literal["bf16", "fp32"] = (
-        "bf16"  # We recommend training with bf16 if possible on your setup
-    )
-    lr_scheduler: Literal["linear", "linear_warmdown"] = "linear_warmdown"
-
-    epochs_per_eval: float | None = (
-        1  # Only one of epochs per eval or steps per eval can be set. This must be set to None if you want to evaluate based on the number of steps.
-    )
-    steps_per_eval: int | None = None
-    epochs_per_save: float | None = None
-    steps_per_save: int | None = None
+    epochs: int = 10
 
     learning_rate: float = 1e-4
     weight_decay: float = 0.1
     warm_up_steps: int = 2000
 
     model_name: str | None = None
+    num_proc_dataset_creation: int = 4
 
     num_entities: int = 2000
     num_relations: int = 200
@@ -64,55 +39,35 @@ class TrainingArgs(BaseModel):
     proportion_ood_facts: float = 0.05
     proportion_iid_test_set_facts: float = 0.005
 
-    proportion_deleted_atomic_facts: float = 0.0
-    proportion_deleted_inferred_test_set_facts: float = 0.1
-
-    n_layer: int | None = 8
-    n_head: int | None = None
-    n_inner: int | None = None
-
 
 def main(args: TrainingArgs):
-    validate_args(args)
+    if args.model_name is None:
+        config = GPT2Config()
+        model = GPT2LMHeadModel(config=config)
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        tokenizer.pad_token = tokenizer.eos_token  # type: ignore
+    else:
+        config = AutoConfig.from_pretrained(args.model_name)
+        model = AutoModelForCausalLM.from_pretrained(args.model_name, config=config)
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
-    experiment_name = get_experiment_name(args)
-    experiment_output_dir = Path(args.output_dir) / experiment_name
-    experiment_output_dir.mkdir(parents=True, exist_ok=True)
+    model, tokenizer, config = (
+        cast(GPT2LMHeadModel, model),
+        cast(PreTrainedTokenizer, tokenizer),
+        cast(PretrainedConfig, config),
+    )  # transformers library isn't fully typed, so we cast to the correct types. Gpt2LMHeadModel can fit in for a wide variety of transformer models
 
-    print(f"Outputs saved at: {experiment_output_dir}")
-
-    # Save the arguments to a file
-    json.dump(
-        obj=args.model_dump(),
-        fp=open(experiment_output_dir / "args.json", "w"),
-        indent=3,
+    train_dataset, test_dataset = get_dataset(
+        tokenizer=tokenizer,
+        num_proc=args.num_proc_dataset_creation,
+        num_entities=args.num_entities,
+        num_relations=args.num_relations,
+        relations_per_entity=args.relations_per_entity,
+        phi=args.phi,
+        proportion_ood_facts=args.proportion_ood_facts,
+        proportion_iid_test_set_facts=args.proportion_iid_test_set_facts,
     )
-
-    setup_logging(experiment_output_dir=experiment_output_dir)
-
-    log().add_to_log_dict(training_args=args)
-
-    model, tokenizer, config = get_model_tokenizer_config(args)
-
-    train_dataset, test_dataset, new_tokens = (
-        get_datasets_and_add_new_tokens_to_model_and_tokenizer(
-            tokenizer=tokenizer,
-            model=model,
-            experiment_output_dir=experiment_output_dir,
-            num_proc=args.num_workers_dataset_creation,
-            num_entities=args.num_entities,
-            num_relations=args.num_relations,
-            relations_per_entity=args.relations_per_entity,
-            phi=args.phi,
-            proportion_ood_facts=args.proportion_ood_facts,
-            proportion_deleted_atomic_facts=args.proportion_deleted_atomic_facts,
-            proportion_deleted_inferred_test_set_facts=args.proportion_deleted_inferred_test_set_facts,
-            proportion_iid_test_set_facts=args.proportion_iid_test_set_facts,
-            data_dir=Path(args.dataset_dir),
-        )
-    )
-
-    log().add_to_log_dict(config=config, new_tokens=new_tokens)
+    model.to("cuda" if torch.cuda.is_available() else "cpu")  # type: ignore
 
     train(
         model=model,
@@ -122,89 +77,16 @@ def main(args: TrainingArgs):
         batch_size=args.batch_size,
         learning_rate=args.learning_rate,
         epochs=args.epochs,
-        max_steps=args.max_steps,  #
-        epochs_per_eval=args.epochs_per_eval,
-        steps_per_eval=args.steps_per_eval,
-        weight_decay=args.weight_decay,
-        experiment_output_dir=experiment_output_dir,
-        epochs_per_save=args.epochs_per_save,
-        steps_per_save=args.steps_per_save,
-        num_workers=args.num_workers,
-        prefetch_factor=args.prefetch_factor,
-        num_warmup_steps=args.warm_up_steps,
-        float_type=args.float_type,
-        lr_scheduler=args.lr_scheduler,
     )
-
-
-DTYPES = {
-    "bf16": torch.bfloat16,
-    "fp32": torch.float32,
-}
-
-
-def get_model_tokenizer_config(
-    args: TrainingArgs,
-) -> tuple[GPT2LMHeadModel, PreTrainedTokenizer, PretrainedConfig]:
-    if args.model_name is None:
-        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")  # type: ignore
-        tokenizer.pad_token = tokenizer.eos_token  # type: ignore
-        kwargs = {}
-        if args.n_layer is not None:
-            kwargs["n_layer"] = args.n_layer
-        if args.n_head is not None:
-            kwargs["n_head"] = args.n_head
-
-        config = GPT2Config(
-            n_inner=args.n_inner,
-            vocab_size=tokenizer.vocab_size,  # type: ignore
-            pad_token_id=tokenizer.pad_token_id,
-            **kwargs,
-        )
-
-        model = GPT2LMHeadModel(config=config)
-    else:
-        config = AutoConfig.from_pretrained(args.model_name)
-        model = AutoModelForCausalLM.from_pretrained(args.model_name, config=config)
-        tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-
-    model.to("cuda" if torch.cuda.is_available() else "cpu")  # type: ignore
-    model.to(DTYPES[args.float_type])  # type: ignore
-
-    return model, tokenizer, config  # type: ignore
-
-
-def validate_args(args: TrainingArgs):
-    assert args.epochs_per_eval is None or args.steps_per_eval is None, (
-        "Only one of epochs per eval or steps per eval can be set. Pass 'None' to the one you don't want to use."
-    )
-    assert args.epochs is None or args.max_steps is None, (
-        "Only one of epochs or num_steps can be set. Pass 'None' to the one you don't want to use."
-    )
-    assert args.steps_per_save is None or args.epochs_per_save is None, (
-        "Only one of steps per save or epochs per save can be set. Pass 'None' to the one you don't want to use."
-    )
-
-
-def get_experiment_name(args: TrainingArgs) -> str:
-    return f"{time.strftime('%Y_%m_%d_%H:%M:%S')}_{args.experiment_name}_phi_{args.phi}_num_entities_{args.num_entities}_num_relations_{args.num_relations}_relations_per_entity_{args.relations_per_entity}_lr_{args.learning_rate}_max_steps_{args.max_steps}"
 
 
 if __name__ == "__main__":
     # Go through and make underscores into dashes, on the cli arguments (for convenience)
-    found_underscore = False
     for arg in sys.argv[1:]:
         if arg.startswith("--"):
-            if not found_underscore:
-                print("Found argument with '_', relacing with '-'")
-                found_underscore = True
-
             sys.argv[sys.argv.index(arg)] = arg.replace("_", "-")
 
     args = CliApp.run(
         TrainingArgs
     )  # Parse the arguments, returns a TrainingArgs object
-    try:
-        main(args)
-    finally:
-        log().write_to_disk()  # Write the log to disk
+    main(args)
