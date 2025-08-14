03bd7c3cd528ae21610aa0b04551ba68c48b124c
levmckinney <levmckinney@gmail.com>
2025-08-14 04:11:04 +0000

    added influence across checkpoints script

---
+++ experiments/baseline_sweep_influence.sh
@@ -0,0 +1,51 @@
+set -e
+
+TARGET_DIR="/mfs1/u/levmckinney/experiments/oocr-inf/outputs/2025_08_14_03-35-05_SWEEP_mKBDr_train_on_fact_documents_train_extractive/2025_08_14_03-35-23_IpWCC_2025_08_14_03-35-05_SWEEP_mKBDr_train_on_fact_documents_train_extractive_index_0_cached_synthetic_docs_hop_num_epochs_1_lr_0.0001"
+
+python -m shared_ml.cli.slurm_sweep \
+    --script_name 'run_influence' \
+    --experiment_name 'influence_on_fact_documents' \
+    --sweep_name 'inf_across_checkpoints' \
+    --target_experiment_dir $TARGET_DIR \
+    --checkpoint_name_sweep '["checkpoint_start", "checkpoint_e1_s97", "checkpoint_e1_s194", "checkpoint_e1_s291", "checkpoint_e1_s388", "checkpoint_final"]' \
+    --query_dataset_split_names '['first_hop_inferred_fact_qa_no_fs', 'second_hop_inferred_fact_qa_no_fs', 'first_hop_inferred_fact_gen_no_fs', 'second_hop_inferred_fact_gen_no_fs', 'name_mayor_eval_gen_no_fs', 'name_mayor_eval_gen_reversed_no_fs', 'name_mayor_eval_qa_no_fs', 'name_mayor_eval_qa_reversed_no_fs']' \
+    --query_batch_size '64' \
+    --query_gradient_rank '64' \
+    --query_gradient_accumulation_steps '10' \
+    --factor_strategy 'ekfac' \
+    --factor_batch_size '2' \
+    --train_batch_size '2' \
+    --covariance_and_lambda_max_examples_sweep '500' \
+    --damping '1e-08' \
+    --layers_to_track 'mlp' \
+    --compute_per_token_scores \
+    --no-compute_per_module_scores \
+    --num_module_partitions_covariance '2' \
+    --num_module_partitions_lambda '2' \
+    --num_module_partitions_scores '1' \
+    --use_half_precision_influence \
+    --dtype_model 'bf16' \
+    --use_flash_attn \
+    --no-torch_distributed \
+    --no-torch_distributed_debug \
+    --dist_nodes '1' \
+    --dist_nproc_per_node 'None' \
+    --distributed_timeout '900' \
+    --random_seed '42' \
+    --num_repeats '1' \
+    --output_dir 'outputs' \
+    --no-overwrite_output_dir \
+    --sweep_output_dir 'outputs' \
+    --sweep_logging_type 'wandb' \
+    --wandb_project 'malign-influence' \
+    --sweep_wandb_project 'malign-influence' \
+    --no-profile_computations \
+    --account 'ml' \
+    --partition 'ml' \
+    --queue 'ml' \
+    --nodes '1' \
+    --nodelist '['concerto1', 'concerto2', 'concerto3']' \
+    --gpus '1' \
+    --cpus_per_task '4' \
+    --memory_gb '320' \
+    --slurm_log_dir 'logs'
