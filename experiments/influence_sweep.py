6e1549b7793ada3f31fb39b0d336ddcf85caf64c
levmckinney <levmckinney@gmail.com>
2025-08-14 01:51:41 +0000

    added sweep train ds sizes

---
+++ experiments/influence_sweep.py
@@ -0,0 +1,104 @@
+import datetime
+import json
+import os
+from pathlib import Path
+from oocr_influence.cli.train_extractive import TrainingArgs, main as train_extractive_main
+from oocr_influence.datasets.synthetic_pretraining_docs._call_models import DocSpec
+from oocr_influence.datasets.synthetic_pretraining_docs._dataset import save_dataset_builders
+from shared_ml.cli.slurm_sweep import run_sweep
+from shared_ml.logging import setup_custom_logging, log
+from oocr_influence.datasets.synthetic_pretraining_docs import load_dataset_builders
+from oocr_influence.datasets.synthetic_pretraining_docs import SyntheticDocsDatasetBuilder
+from numpy.random import default_rng
+from collections import defaultdict
+
+WORKING_DIR = Path(__file__).parent.parent
+os.chdir(WORKING_DIR)
+
+
+SWEEP_NAME = "different_amounts_of_data"
+OUTPUT_DIR = WORKING_DIR / "outputs" / f"{datetime.datetime.now(datetime.timezone.utc).strftime('%Y_%m_%d_%H-%M-%S')}_{SWEEP_NAME}"
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+setup_custom_logging(
+    experiment_name=f"sweep_logs",
+    experiment_output_dir=OUTPUT_DIR,
+    logging_type="wandb",
+    wandb_project="malign-influence",
+)
+log().add_to_log_dict(
+    sweep_id=SWEEP_NAME,
+)
+
+DATASET_BUILDER_PATH = Path("/mfs1/u/levmckinney/data/oocr-inf/dataset_builders_plausible.json")
+PERCENTAGES = [0.15, 0.66666, 1]
+
+
+synthetic_docs, eval_builders, metadata = load_dataset_builders(DATASET_BUILDER_PATH)
+
+rng = default_rng(43)
+fact_id_to_builders = defaultdict(list)
+for builder in synthetic_docs.docs:
+    fact_id = builder.fact.id
+    fact_id_to_builders[fact_id].append(builder)
+
+dataset_builder_paths = []
+for percentage in PERCENTAGES:
+    doc_builders = []
+    for fact_id in fact_id_to_builders.keys():
+        builders = rng.permutation(fact_id_to_builders[fact_id]).tolist()
+        k = int(len(builders)*(percentage/100))
+        doc_builders.extend(builders[:k])
+
+    docs_included = []
+    for doc in doc_builders:
+        doc_spec = DocSpec.model_dump(doc)
+        del doc_spec["text"]
+        docs_included.append(doc_spec)
+
+    metadata = {
+        "docs_included": docs_included,
+    }
+    save_dataset_builders(
+        SyntheticDocsDatasetBuilder(
+            docs=doc_builders,
+        ),
+        eval_builders,
+        OUTPUT_DIR / f"dataset_builder_{percentage}.json",
+        metadata,
+    )
+    dataset_builder_paths.append(OUTPUT_DIR / f"dataset_builder_{percentage}.json")
+    # Also save metadata separately
+    with open(OUTPUT_DIR / f"metadata_{percentage}.json", "w") as f:
+        json.dump(metadata, f)
+
+train_args_list = []
+for builder_path in dataset_builder_paths:
+    train_args = TrainingArgs(
+        experiment_name="group_inf_estimation",
+        output_dir=OUTPUT_DIR,
+        synth_dataset_builders_path=builder_path,
+        fact_dataset_type="cached_synthetic_docs",
+        weight_decay=0.1,
+        learning_rate=0.0001,
+        warmup_proportion=0.1,
+        epochs=1,
+        eval_first_step=True,
+        epochs_per_save=None,
+        epochs_per_eval=1,
+        batch_size=8,
+        save_final_checkpoint=True,
+        logging_type="disk",
+        micro_batch_size=1,
+    )
+    train_args_list.append(train_args.model_dump())
+
+run_id = run_sweep(
+    target_args_model=TrainingArgs,  # type: ignore
+    target_entrypoint=train_extractive_main,
+    arguments=train_args_list,
+    parallelism_limit=2,
+    sweep_name="group_data_modeling",
+    nodelist=["overture", "concerto1", "concerto2", "concerto3"],
+    sweep_id="group_data_modeling",
+    force_git_repo_has_sweep=True,
+)
